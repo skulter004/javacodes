17b296e69b7bdfcceb1c4777ce5943c79c79976b
shivam singh <87563962+skulter004@users.noreply.github.com>
2022-04-20 23:58:49 +0530

    Create 2darray.java

---
+++ 2darray.java
@@ -0,0 +1,16 @@
+  public static void main(String[] args) {
+            int arr[][]= new int[4][4];
+    
+            for(int i=0;i<4;i++){
+                for(int j=0;j<4;j++){
+                    arr[i][j]=((i+1)*(j+1));
+                }
+            }
+        
+            
+            System.out.print(arr[3][3]+" ");
+
+        
+    
+    System.out.println();
+         }
