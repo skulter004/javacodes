ee1bd1824843ab2e6828ada7b0cbe716250601a8
shivam singh <87563962+skulter004@users.noreply.github.com>
2022-04-20 23:56:56 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,10 @@
+  public static void main(String[] args){
+        System.out.println("hello there");
+        int n = 5;
+        for(int i=0;i<n;i++){
+            for (int j=1;j<=n-i;j++){
+             System.out.print("*");
+        }
+        System.out.println();
+    }
+}
